5d2d045bd9afc01b817b87c63d15821861209cfc
Charly Abraham <charly@core.ai>
2024-11-22 16:58:45 +0530

    fix: exit process once execution is completed and handle url redirects

--- src/download.js
+++ src/download.js
@@ -1,9 +1,19 @@
-import {downloadNodeBinary, fetchLatestNodeVersion} from "./nodeDownloader.js";
-import platforms from "./platforms.json" assert {type: 'json'};
+import { downloadNodeBinary, fetchLatestNodeVersion } from "./nodeDownloader.js";
+import platforms from "./platforms.json" assert { type: 'json' };
 
-const supportedPlatforms = platforms.configurations;
-const version = await fetchLatestNodeVersion();
-for (const platform of supportedPlatforms) {
-    const fileName = await downloadNodeBinary(version, platform.platform, platform.arch);
-    console.log(fileName);
-}
+(async () => {
+    try {
+        const supportedPlatforms = platforms.configurations;
+        const version = await fetchLatestNodeVersion();
+
+        for (const platform of supportedPlatforms) {
+            const fileName = await downloadNodeBinary(version, platform.platform, platform.arch);
+            console.log(fileName);
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+    } finally {
+        process.exit(0); // Ensure the process exits properly
+    }
+})();
+
